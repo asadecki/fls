a986220591c50e4825a00b1763ae1079da384b83
Aleksander Sadecki <aleksander.sadecki@schibsted.pl>
2015-04-30 12:27:10 +0200

    mock frontend

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
 console.log('\'Allo \'Allo!');
+
+$.ajax({
+    url: "http://liga-fls.pl/home/index.php?option=com_joomleague&view=roster&p=84:liga-c1-wiosna-2015&tid=21:ubs&division=0&Itemid=564",
+    success: function(data) {
+        alert('xxx');
+    },
+    crossDomain: true,
+    dataType: 'jsonp'
+});
